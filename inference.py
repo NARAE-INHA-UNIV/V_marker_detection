1006048a4f8477ec4602fce9c6dff1cc6c3492d9
서보성 <sbosung0610@gmail.com>
2026-01-23 15:34:22 +0900

    1주차 숙제(inference.py 수정)

--- inference.py
+++ inference.py
@@ -15,6 +15,7 @@ def measure_performance(model_name, model_path, video_path):
 
     total_inference_time = 0
     inference_time = 0
+    encoding_time = 0
     frame_count = 0
 
     while video.isOpened():
@@ -25,25 +26,30 @@ def measure_performance(model_name, model_path, video_path):
             break
 
         start_inf = time.perf_counter() # 추론 시작
-        results = model(frame, verbose=False, device="cpu")
+        results = model(frame, verbose=False, device=0) # Colab GPU
         end_inf = time.perf_counter() # 추론 끝
 
-        annotated_frame = results[0].plot()
+        annotated_frame = results[0].plot() # 결과 그림 그리기
+
+        start_enc = time.perf_counter() # 인코딩 시작
         out.write(annotated_frame)
+        end_enc = time.perf_counter() # 인코딩 끝
 
         end_total = time.perf_counter() # 전체 끝
 
         total_inference_time += (end_total - start_total)
         inference_time += (end_inf - start_inf)
+        encoding_time += (end_enc - start_enc)
         frame_count += 1
-    
+
     video.release()
     out.release()
 
     total_fps = frame_count / total_inference_time
     inf_fps = frame_count / inference_time
+    enc_fps = frame_count / encoding_time
 
-    return total_fps, inf_fps
+    return total_fps, inf_fps, enc_fps
 
 if __name__ == "__main__":
 
@@ -59,8 +65,8 @@ if __name__ == "__main__":
     results = {}
 
     for model, path in models.items():
-        total_fps, inf_fps = measure_performance(model, path, video_path=video_path)
-        results[model] = f"total_fps: {total_fps}, inf_fps: {inf_fps}"
+        total_fps, inf_fps, enc_fps = measure_performance(model, path, video_path=video_path)
+        results[model] = f"total_fps: {total_fps}, inf_fps: {inf_fps}, enc_fps: {enc_fps}"
 
     print(json.dumps(results, indent=4))
                           
